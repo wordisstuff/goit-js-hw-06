5a7a8d9baf94b91fb1b668150c7c0dc8e1154d4b
wordisstuff <wordisstuff@gmail.com>
2023-10-07 17:47:01 +0300

    fix task 9

--- js/task-09.js
+++ js/task-09.js
@@ -5,9 +5,12 @@ const text = body.querySelector('.color')
 btnChengeColor.addEventListener('click', onBtn)
 function onBtn(event) {
   event.preventDefault();
-  body.style.backgroundColor = getRandomHexColor();
+  const colorChenge = getRandomHexColor()
+  body.style.backgroundColor = colorChenge;
+  text.style.color = colorChenge;
   text.textContent = body.style.backgroundColor;
-  text.style.color = getRandomHexColor();
+  text.style.backgroundColor = '#ffffff';
+  text.style.borderRadius = '5px';
 };
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
